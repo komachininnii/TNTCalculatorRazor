b9a68e1f6e3d7b67b0b31549563580ecc293d25e
Takahiko Yamanoi <ninnii@live.jp>
2026-03-05 19:02:08 +0900

    TestCasesにコメント付与

--- TNTCalculatorRazor.Tests/TestData/EnteralFormulaTestCases.cs
+++ TNTCalculatorRazor.Tests/TestData/EnteralFormulaTestCases.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 using TNTCalculatorRazor.Domain.Enums;
 
 namespace TNTCalculatorRazor.Tests.TestData;
@@ -10,13 +11,13 @@ public static class EnteralFormulaTestCases
     // 経腸栄養剤のテストケース
     public static IEnumerable<object[]> CurrentFormulas_WithPackKcalAndVolume()
     {
-        yield return new object[] { EnteralFormulaType.Meibalance10, 400.0, 400.0 };
-        yield return new object[] { EnteralFormulaType.PeptamenPrebio15, 400.0, 267.0 };
-        yield return new object[] { EnteralFormulaType.PeptamenIntense10, 200.0, 200.0 };
-        yield return new object[] { EnteralFormulaType.PeptamenAF15, 300.0, 200.0 };
-        yield return new object[] { EnteralFormulaType.IsocalSupport15, 400.0, 267.0 };
+        yield return new object[] { EnteralFormulaType.Meibalance10, 400.0, 400.0 };        // 400kcal / 400mL
+        yield return new object[] { EnteralFormulaType.PeptamenPrebio15, 400.0, 267.0 };    // 400kcal / 267mL
+        yield return new object[] { EnteralFormulaType.PeptamenIntense10, 200.0, 200.0 };   // 200kcal / 200mL
+        yield return new object[] { EnteralFormulaType.PeptamenAF15, 300.0, 200.0 };        // 300kcal / 200mL
+        yield return new object[] { EnteralFormulaType.IsocalSupport15, 400.0, 267.0 };     // 400kcal / 267mL
         yield return new object[] { EnteralFormulaType.Lacphia15, 400.0, 267.0 };
-        yield return new object[] { EnteralFormulaType.Mein10, 200.0, 200.0 };
+        yield return new object[] { EnteralFormulaType.Mein10, 200.0, 200.0 }; 
         yield return new object[] { EnteralFormulaType.RenalenMP16, 400.0, 250.0 };
         yield return new object[] { EnteralFormulaType.GlucernaRex10, 400.0, 400.0 };
         yield return new object[] { EnteralFormulaType.PGSoftEJ15, 400.0, 267.0 };
